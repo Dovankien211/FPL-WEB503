3ae3b299aca81ee89c041d891b5758618a6ca99b
datlt2306 <datlt2306@gmail.com>
2025-01-08 08:02:47 +0700

    add product routing to app.js with sample data

--- app.js
+++ app.js
@@ -2,8 +2,17 @@ import express from "express";
 
 const app = express();
 
-app.listen(() => {
-    console.log("Server is running");
+// data fake
+const data = [
+    { id: 1, name: "Product A", price: 100 },
+    { id: 2, name: "Product A", price: 200 },
+    { id: 3, name: "Product C", price: 200 },
+];
+// routing của express hỗ trợ
+app.get("/products", (req, res) => {
+    console.log("req.query", req.query);
+    console.log("req:params", req.params);
+    res.status(200).json(data);
 });
 
 export const viteNodeApp = app;
