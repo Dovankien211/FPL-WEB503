cdc0c2307d46bc8eea3e9b684f71d4a653aa1a51
datlt2306 <datlt2306@gmail.com>
2025-01-08 08:46:47 +0700

    add product retrieval and creation endpoints with error handling

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import express from "express";
 
 const app = express();
 
+// middleware
+app.use(express.json());
 // data fake
 const data = [
     { id: 1, name: "Product A", price: 100 },
@@ -10,9 +12,26 @@ const data = [
 ];
 // routing của express hỗ trợ
 app.get("/products", (req, res) => {
-    console.log("req.query", req.query);
-    console.log("req:params", req.params);
-    res.status(200).json(data);
+    return res.status(200).json(data);
+});
+
+app.get("/products/:id", (req, res) => {
+    const product = data.find((item) => item.id === +req.params.id);
+    if (!product) {
+        return res.status(404).json({
+            message: "Không có sản phẩm nào!",
+        });
+    }
+    return res.status(200).json(product);
+});
+app.post("/products", (req, res) => {
+    if (!req.body) {
+        return res.status(400).json({
+            message: "Dữ liệu không hợp lệ!",
+        });
+    }
+    const products = [...data, req.body];
+    return res.status(201).json(products);
 });
 
 export const viteNodeApp = app;
