6e01cf798fb9157f08891f07c816666cc3744d0b
datlt2306 <datlt2306@gmail.com>
2025-01-10 08:45:55 +0700

    add product management endpoints with detailed documentation and error handling

--- app.js
+++ app.js
@@ -2,36 +2,85 @@ import express from "express";
 
 const app = express();
 
-// middleware
 app.use(express.json());
-// data fake
+
 const data = [
-    { id: 1, name: "Product A", price: 100 },
-    { id: 2, name: "Product A", price: 200 },
-    { id: 3, name: "Product C", price: 200 },
+    { id: 1, name: "A", price: 100 },
+    { id: 2, name: "B", price: 200 },
+    { id: 3, name: "C", price: 300 },
 ];
-// routing của express hỗ trợ
+/**
+ * @route   GET /products
+ * @desc    Lấy toàn bộ danh sách sản phẩm
+ * @access  Public
+ * @returns {Array} Danh sách sản phẩm
+ *
+ * Response Example:
+ * [
+ *     { "id": 1, "name": "Product A", "price": 100 },
+ *     { "id": 2, "name": "Product B", "price": 200 },
+ *     { "id": 3, "name": "Product C", "price": 300 },
+ *     { "id": 4, "name": "Product D", "price": 400 }
+ * ]
+ */
 app.get("/products", (req, res) => {
-    return res.status(200).json(data);
+    res.json(data);
+});
+/**
+ * @route   POST /products
+ * @desc    Thêm một sản phẩm mới vào danh sách
+ * @access  Public
+ * @param   {Object} req.body - Dữ liệu sản phẩm được gửi trong body của request
+ * @property {number} id - ID của sản phẩm (tuỳ chọn)
+ * @property {string} name - Tên của sản phẩm (bắt buộc)
+ * @property {number} price - Giá của sản phẩm (bắt buộc)
+ * @returns {Object} Thông tin sản phẩm vừa được thêm hoặc thông báo lỗi
+ * */
+app.post("/products", (req, res) => {
+    const { id, name, price } = req.body;
+    if (!name || !price) {
+        return res.status(400).json({
+            message: "Thiếu trường dữ liệu",
+        });
+    }
+    const existProduct = data.find((item) => item.id === id);
+    if (existProduct) {
+        return res.status(400).json({
+            message: "Sản phẩm đã tồn tại",
+        });
+    }
+    const newProduct = { id, name, price };
+    data.push(newProduct);
+
+    return res.status(201).json(newProduct);
 });
 
-app.get("/products/:id", (req, res) => {
-    const product = data.find((item) => item.id === +req.params.id);
+app.delete("/products/:id", (req, res) => {
+    const { id } = req.params;
+    const product = data.find((item) => item.id === +id);
     if (!product) {
         return res.status(404).json({
-            message: "Không có sản phẩm nào!",
+            message: "Sản phẩm không tồn tại!",
         });
     }
-    return res.status(200).json(product);
+    // xóa sản phẩm
+    data.filter((product) => product.id !== +id);
+    return res.status(200).json({
+        message: "Xóa sản phẩm thành công",
+        data: product,
+    });
 });
-app.post("/products", (req, res) => {
-    if (!req.body) {
-        return res.status(400).json({
-            message: "Dữ liệu không hợp lệ!",
-        });
-    }
-    const products = [...data, req.body];
-    return res.status(201).json(products);
-});
-
 export const viteNodeApp = app;
+
+// Viết API nào thì test luôn API đấy
+/**
+ * Delete
+ * B1: Định nghĩa router
+ *  - METHOD: DELETE
+ *  - URL: /products/:id
+ * B2: Xử lý logic
+ *  - Lấy id từ params
+ *  - tìm sản phẩm trong db dựa theo id, nếu không có sp thông báo không tìm thấy sản phẩm
+ *  - Sử dụng method filter để lọc ra những phần tử khác với id
+ *  - nếu thành công trả về status 200 và thông báo xóa thành công
+ */
