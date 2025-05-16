6dce2299d6d6f181fc25a277abe3d9a0f15df255
datlt2306 <datlt2306@gmail.com>
2025-05-16 15:41:47 +0700

    feat(app): thêm middleware và cải thiện router cho API
    - Thêm middleware `express.json()` để xử lý dữ liệu JSON trong yêu cầu.
    - Thêm middleware `morgan` để ghi lại log các yêu cầu HTTP.
    - Cập nhật các route trong `posts.js` để trả về dữ liệu JSON thay vì chỉ log ra console.
    - Cải thiện cấu trúc mã nguồn để dễ dàng quản lý và mở rộng trong tương lai.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
 import router from "./routers";
-
+import morgan from "morgan";
 dotenv.config();
 const app = express();
 
+// middleware
+app.use(express.json());
+// logger
+app.use(morgan("dev"));
+
+// router
 app.use("/api", router);
 
 app.listen(process.env.PORT, () => {
--- src/routers/posts.js
+++ src/routers/posts.js
@@ -2,14 +2,16 @@ import express from "express";
 
 const routePosts = express.Router();
 
-routePosts.get("/", () => {
-    console.log("Trả về danh sách bài viết");
+routePosts.get("/", (req, res) => {
+    res.json({
+        message: "Danh sách bài viết",
+    });
 });
 routePosts.get("/:id", () => {
     console.log("Trả một bài viết");
 });
-routePosts.post("/", () => {
-    console.log("Thêm bài viết");
+routePosts.post("/", (req, res) => {
+    console.log(req.body);
 });
 routePosts.put("/", () => {
     console.log("Cập nhật bài viết");
