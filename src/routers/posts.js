37a3c68b980fb500cf8c421dae0787fb04d6243e
datlt2306 <datlt2306@gmail.com>
2025-05-19 14:58:56 +0700

    feat(posts): thêm chức năng lấy danh sách và chi tiết bài viết
    - Cập nhật route để trả về danh sách bài viết dưới dạng JSON.
    - Thêm chức năng lấy chi tiết bài viết theo ID, xử lý trường hợp không tìm thấy bài viết.
    - Cải thiện cấu trúc mã nguồn để dễ dàng mở rộng trong tương lai.

--- src/routers/posts.js
+++ src/routers/posts.js
@@ -1,14 +1,20 @@
 import express from "express";
 
 const routePosts = express.Router();
-
+let posts = [
+    { id: 1, title: "Bài viết 1", content: "Nội dung bài viết 1" }, // 1 ==
+    { id: 2, title: "Bài viết 2", content: "Nội dung bài viết 2" }, // p
+];
 routePosts.get("/", (req, res) => {
-    res.json({
-        message: "Danh sách bài viết",
-    });
+    res.json(posts);
 });
-routePosts.get("/:id", () => {
-    console.log("Trả một bài viết");
+routePosts.get("/:id", (req, res) => {
+    const post = posts.find((p) => p.id === +req.params.id);
+    if (!post)
+        return res.status(404).json({
+            message: "Không có bài viết nào!",
+        });
+    return res.json(post);
 });
 routePosts.post("/", (req, res) => {
     console.log(req.body);
