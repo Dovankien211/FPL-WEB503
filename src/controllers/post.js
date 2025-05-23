dab6860e63f672abd916642c3936a34efa1e190a
datlt2306 <datlt2306@gmail.com>
2025-05-23 12:44:09 +0700

    feat(posts): implement CRUD operations for posts with proper error handling

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -14,7 +14,48 @@ export const getAllPosts = async (req, res) => {
         });
     }
 };
-export const getPostById = async (req, res) => {};
-export const createPost = async (req, res) => {};
-export const updatePost = async (req, res) => {};
-export const deletePost = async (req, res) => {};
+export const getPostById = async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if(!post){
+            return res.status(404).json({
+                message: "Không tìm thấy bài viết",
+            });
+        }
+        return res.json(post)
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message,
+        });
+    }
+};
+export const createPost = async (req, res) => {
+    try {
+        const post = await Post.create(req.body);
+        return res.status(201).json(post);
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message,
+        });
+    }
+};
+export const updatePost = async (req, res) => {
+    try {
+       const post =  await Post.findByIdAndUpdate(req.params.id, req.body, { new: true})
+       return res.json(post);
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message,
+        });
+    }
+};
+export const deletePost = async (req, res) => {
+    try {
+        await Post.findByIdAndDelete(req.params.id);
+        return res.json({ success: true});
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message,
+        });
+    }
+};
--- src/routers/posts.js
+++ src/routers/posts.js
@@ -1,49 +1,12 @@
 import { Router } from "express";
-import { getAllPosts } from "../controllers/post";
+import { createPost, deletePost, getAllPosts, getPostById, updatePost } from "../controllers/post";
 
 const routePost = Router();
-// Datafake
-let posts = [
-    { id: 1, title: "Bài viết 1", content: "Nội dung bài viết 1" },
-    { id: 2, title: "Bài viết 2", content: "Nội dung bài viết 2" },
-];
-// api/posts
+
 routePost.get("/", getAllPosts);
+routePost.get("/:id", getPostById);
+routePost.post("/",  createPost);
+routePost.put("/:id", updatePost);
+routePost.delete("/:id", deletePost);
 
-// api/posts/:id
-routePost.get("/:id", (req, res) => {
-    const post = posts.find((p) => p.id === +req.params.id);
-    if (!post)
-        return res.status(404).json({
-            message: "Không có bài viết nào phù hợp",
-        });
-    return res.json(post);
-});
-routePost.post("/", (req, res) => {
-    const { title, content } = req.body;
-    const newPost = { id: Date.now(), title: title, content: content };
-    posts.push(newPost);
-    return res.status(201).json(newPost);
-});
-
-routePost.put("/:id", (req, res) => {
-    const post = posts.find((p) => p.id === +req.params.id);
-    if (!post)
-        return res.status(404).json({
-            message: "Không có bài viết nào phù hợp",
-        });
-    const { title, content } = req.body;
-    post.title = title || post.title;
-    post.content = content || post.content;
-
-    return res.json(post);
-});
-
-routePost.delete("/:id", (req, res) => {
-    const index = posts.findIndex((p) => p.id == req.params.id);
-    if (index === -1) return res.status(404).json({ error: "Post not found" });
-
-    posts.splice(index, 1);
-    res.json({ success: true });
-});
 export default routePost;
