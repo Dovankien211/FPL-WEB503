b42fbc816c6163e4b8f0b95cba66c4d8a5856d92
datlt2306 <datlt2306@gmail.com>
2025-05-26 15:00:10 +0700

    feat(products): thêm chức năng quản lý sản phẩm
    - Thêm các route cho chức năng CRUD sản phẩm, bao gồm lấy danh sách, lấy chi tiết, thêm, cập nhật và xóa sản phẩm.
    - Cập nhật cấu trúc router để tích hợp các route sản phẩm mới.
    - Thêm thư viện `joi` vào dự án để hỗ trợ xác thực dữ liệu đầu vào cho các yêu cầu liên quan đến sản phẩm.

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,8 +1,13 @@
-import express from "express";
-import routePosts from "./posts";
+import { Router } from "express";
+import routePost from "./posts";
+import routeProduct from "./product";
 
-const router = express.Router();
+const router = Router();
 
-router.use("/posts", routePosts);
+// Sử dụng router cho bài viết
+router.use("/posts", routePost);
+
+// Sử dụng router cho sản phẩm
+router.use("/products", routeProduct);
 
 export default router;
