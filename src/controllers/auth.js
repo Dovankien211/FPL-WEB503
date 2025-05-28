1bec78e6222dc44dbd004c28868ed7e4de66e791
datlt2306 <datlt2306@gmail.com>
2025-05-28 15:56:46 +0700

    feat(auth): thêm chức năng đăng nhập cho người dùng
    - Thêm hàm `signin` để xử lý yêu cầu đăng nhập, bao gồm xác thực email và mật khẩu.
    - Sử dụng `jsonwebtoken` để tạo token cho người dùng sau khi đăng nhập thành công.
    - Cập nhật file `auth.js` và `authRouter` để tích hợp route `/signin` cho chức năng đăng nhập.

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,5 +1,6 @@
 import User from "../models/user";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res) => {
     try {
@@ -16,6 +17,30 @@ export const signup = async (req, res) => {
             user,
         });
     } catch (error) {
-        res.status(400).json({ error: "Lỗi khi đăng ký", message: error.message });
+        return res.status(400).json({ error: "Lỗi khi đăng ký", message: error.message });
+    }
+};
+
+export const signin = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email }).select("+password");
+        if (!user) {
+            return res.status(400).json({
+                message: "Email không tồn tại",
+            });
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch)
+            return res.status(400).json({
+                message: "Mật khẩu không đúng!",
+            });
+        const token = jwt.sign({ id: user._id }, "123456", { expiresIn: "1h" });
+        return res.status(200).json({
+            message: "Đăng nhập thành công",
+            token,
+        });
+    } catch (error) {
+        return res.status(400).json({ error: "Lỗi khi đăng nhập", message: error.message });
     }
 };
--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { signup } from "../controllers/auth";
+import { signin, signup } from "../controllers/auth";
 import { validateRequest } from "../middlewares/validateRequest";
 import { signupSchema } from "../validations/auth";
 
 const authRouter = express.Router();
-// POST api/auth/signup
 authRouter.post("/signup", validateRequest(signupSchema), signup);
+authRouter.post("/signin", signin);
 
 export default authRouter;
 
