9d120bc628d9d869e7271e3bedef4b0494c37f4c
datlt2306 <datlt2306@gmail.com>
2025-05-28 13:25:55 +0700

    feat(auth): thêm xác thực dữ liệu cho yêu cầu đăng ký người dùng
    - Thêm middleware `validateRequest` để xác thực dữ liệu đầu vào cho yêu cầu đăng ký người dùng.
    - Sử dụng `signupSchema` để đảm bảo dữ liệu đầu vào hợp lệ trước khi xử lý yêu cầu đăng ký.

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,7 +1,9 @@
 import { signup } from "../controllers/auth";
 import express from "express";
+import { validateRequest } from "../middlewares/validateRequest";
+import { signupSchema } from "../validations/auth";
 
 const authRouter = express.Router();
 
-authRouter.post("/signup", signup);
+authRouter.post("/signup", validateRequest(signupSchema), signup);
 export default authRouter;
