11c4d6eb9088b64c76dd83f925b65d4901c5b6df
datlt2306 <datlt2306@gmail.com>
2025-07-03 09:10:22 +0700

    feat(app): khởi tạo ứng dụng Express và thêm router cho bài viết
    - Thêm cấu hình cơ bản cho ứng dụng Express.
    - Tạo router cho các API liên quan đến bài viết.
    - Thêm endpoint GET /hello để trả về lời chào.
    - Thêm endpoint GET /posts/:id để lấy thông tin chi tiết bài viết.

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+import postRouter from "./routers/posts";
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use("/api/posts", postRouter);
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
