80f91ab7172edf30eb18c8ca67d01ca29b01c35a
datlt2306 <datlt2306@gmail.com>
2025-08-07 17:39:33 +0700

    feat(posts): thêm tính năng tạo bài viết mới qua API</message>
    <message>
    - Thêm middleware `express.json()` vào ứng dụng để xử lý dữ liệu JSON.
    - Cập nhật router bài viết với endpoint POST `/api/posts` cho phép thêm bài viết mới.
    - Trả về thông báo thành công cùng với dữ liệu bài viết đã thêm.

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+
 app.use("/api/posts", postRouter);
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
--- src/routers/posts.js
+++ src/routers/posts.js
@@ -2,14 +2,22 @@ import { Router } from "express";
 
 const postRouter = Router();
 
+// GET api/posts
 postRouter.get("/", (req, res) => {
     return res.json({
         message: "Danh sách bài viết",
     });
 });
-postRouter.get("/:id", (req, res) => {
-    return res.json({
-        message: "Chi tiết bài viết",
+// POST api/posts
+postRouter.post("/", (req, res) => {
+    // const title = req.body.title;
+    // const content = req.body.content;
+    const { title, content } = req.body;
+
+    return res.status(201).json({
+        message: "Thêm bài viết thành công",
+        data: { title, content },
     });
 });
 export default postRouter;
+// Client -> submit form (form data) =>middleware(express.json()) =>server nhận dữ liệu (req.body)
