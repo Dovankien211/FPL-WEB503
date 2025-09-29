2d387fe5cf7e4cc97fbb55a396af4ef67e949f69
datlt2306 <datlt2306@gmail.com>
2025-09-29 19:21:02 +0700

    feat(auth): thêm router xác thực người dùng
    - Thêm router mới cho xác thực người dùng vào ứng dụng.
    - Cập nhật cấu trúc router chính để bao gồm router xác thực.
    - Cải thiện khả năng tổ chức mã nguồn và dễ dàng mở rộng trong tương lai.

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import postRouter from "./posts";
+import authRouter from "./auth";
 
 const router = Router();
 
 // router bài viết
 
 router.use("/posts", postRouter);
+router.use("/auth", authRouter);
 export default router;
