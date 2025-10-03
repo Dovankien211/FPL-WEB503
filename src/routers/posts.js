7cb047b6986d433fec24706a2bddc841b0bc3b3f
datlt2306 <datlt2306@gmail.com>
2025-10-03 19:25:18 +0700

    feat(posts): thêm xác thực cho các yêu cầu bài viết
    - Thêm middleware `verifyJWT` để xác thực người dùng trước khi thực hiện các thao tác trên bài viết.
    - Thêm middleware `restrictTo` để giới hạn quyền truy cập cho các vai trò "admin" và "staff".
    - Cải thiện tính bảo mật và quản lý quyền truy cập cho các endpoint liên quan đến bài viết.

--- src/routers/posts.js
+++ src/routers/posts.js
@@ -8,12 +8,17 @@ import {
 } from "../controllers/post.controller";
 import validateRequest from "../middlewares/validateRequest";
 import { postSchema } from "../validation/post.validation";
+import { restrictTo, verifyJWT } from "../middlewares/auth.middleware";
 const postRouter = Router();
 
 // Lấy danh sách
 postRouter.get("/", getPosts);
 // Lấy chi tiết bài viết
 postRouter.get("/:id", getPostById);
+
+postRouter.use(verifyJWT);
+
+postRouter.use(restrictTo("admin", "staff"));
 // Thêm bài viết
 postRouter.post("/", validateRequest(postSchema), createPost);
 // Cập nhật
