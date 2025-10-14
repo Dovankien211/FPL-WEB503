0acd592cfcf9e5cd0f02700474774980cf4cc9e5
Dovankien211 <kiendvph48901@gmail.com>
2025-10-14 10:04:40 +0000

    A4345:04 PM287 40PMve: {10/14/2025, 5:04 PMile} PM5:04 PM {n287w}

--- src/models/post.js
+++ src/models/post.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 const postSchema = new mongoose.Schema({
-    title: {type: String, required: true},
+    name: {type: String, required: true},
     content: {type: String, required: true},
     author: {type: String, required: true},
     createAt: {type: Date, default: Date.now},
